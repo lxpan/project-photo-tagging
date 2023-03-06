e8cdde4eb5416d2a5bcf5435dd6203a8d6887289
Luoxi Pan <l.panpax@gmail.com>
2023-03-06 17:08:04 +1100

    Create starting components

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/Header.css';
+
+export default function Header() {
+    return (
+        <div className="header-container">
+            <h1>Header</h1>
+        </div>
+    );
+}
