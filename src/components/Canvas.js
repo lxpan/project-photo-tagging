55b4bf5d44844067f5b00bf4ca2af1868ce0c977
Luoxi Pan <l.panpax@gmail.com>
2023-03-06 17:20:37 +1100

    Add the Wally photo

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import '../styles/Canvas.css';
+import wallyPhoto from '../assets/photos/steampunk-wally.jpeg';
 
 export default function Canvas() {
     return (
-        <div>
-            <h1>Canvas</h1>
+        <div className="canvas-container">
+            <img src={wallyPhoto} alt="" />
         </div>
     );
 }
