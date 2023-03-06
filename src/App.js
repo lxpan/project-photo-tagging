9c6f9e583101c1517f267ecb0507e71c90431158
Luoxi Pan <l.panpax@gmail.com>
2023-03-06 17:07:50 +1100

    Install firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import React from 'react';
-import './App.css';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import {
+    getFirestore, collection, doc, getDocs, setDoc, deleteDoc,
+} from 'firebase/firestore';
+// Components
+import Header from './components/Header';
+import Canvas from './components/Canvas';
+// CSS
+import './styles/App.css';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: 'AIzaSyDpAI5r_jWjU1aNFIqONr_sdHrDJAX7VP0',
+    authDomain: 'wheres-wally-789b9.firebaseapp.com',
+    projectId: 'wheres-wally-789b9',
+    storageBucket: 'wheres-wally-789b9.appspot.com',
+    messagingSenderId: '906692154300',
+    appId: '1:906692154300:web:327b1ae82ad7d4f58bd1b9',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+console.log(db);
 
 function App() {
-    return <div className="App" />;
+    return (
+        <div className="App">
+            <Header />
+            <Canvas />
+        </div>
+    );
 }
 
 export default App;
