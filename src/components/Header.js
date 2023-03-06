561c264d48b06c15bfb3d9268a81e64d0970db37
Luoxi Pan <l.panpax@gmail.com>
2023-03-06 17:57:39 +1100

    Add custom font for Header title

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,8 @@ import '../styles/Header.css';
 export default function Header() {
     return (
         <div className="header-container">
-            <h1 className="header-title">Wally in Steampunk</h1>
+            <h1 className="header-title-h1">where's wally?</h1>
+            <h2 className="header-title-h2">steampunk edition</h2>
         </div>
     );
 }
