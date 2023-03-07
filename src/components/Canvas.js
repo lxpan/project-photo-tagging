f24fb4200c5056a10eda93246835ac62b615993e
Luoxi Pan <l.panpax@gmail.com>
2023-03-07 15:27:12 +1100

    Add Canvas states to track right clicks

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,16 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/Canvas.css';
 import wallyPhoto from '../assets/photos/steampunk-wally.jpeg';
 
 function Canvas() {
-    console.log('Load Canvas.');
+    const [clicked, setClicked] = useState(false);
+    const [points, setPoints] = useState({
+        x: 0,
+        y: 0,
+    });
+
+    useEffect(() => {
+        const handleClick = () => setClicked(false);
+        window.addEventListener('click', handleClick);
+        return () => {
+            window.removeEventListener('click', handleClick);
+        };
+    }, []);
 
     return (
         <div
             className="canvas-container"
             onContextMenu={(e) => {
                 e.preventDefault();
-                console.log('Right Click');
+                setClicked(true);
+                setPoints({
+                    x: e.pageX,
+                    y: e.pageY,
+                });
+                console.log('Right Click', e.pageX, e.pageY);
             }}
         >
             <img src={wallyPhoto} alt="" />
