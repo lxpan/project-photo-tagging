f817598b8dd0ec60de3aab1ad40c70c37ba804ed
Luoxi Pan <l.panpax@gmail.com>
2023-03-14 16:02:09 +1100

    Add visible bounding boxes for verification

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -3,13 +3,56 @@ import '../styles/Canvas.css';
 import wallyPhoto from '../assets/photos/steampunk-wally.jpeg';
 import MenuContext from './MenuContext';
 
+const boundingBoxes = {
+    deimos: {
+        x: 502,
+        y: 192,
+        w: 120,
+        h: 160,
+    },
+
+    snuffy: {
+        x: 392,
+        y: 962,
+        w: 50,
+        h: 50,
+    },
+
+    leo: {
+        x: 648,
+        y: 1051,
+        w: 80,
+        h: 60,
+    },
+    wally: {
+        x: 1602,
+        y: 683,
+        w: 20,
+        h: 20,
+    },
+};
+
 function Canvas() {
     const img = <img src={wallyPhoto} alt="" />;
 
+    useEffect(() => {
+        const canvas = document.getElementById('canvas');
+        const ctx = canvas.getContext('2d');
+        ctx.strokeStyle = 'lightgreen';
+        ctx.lineWidth = 1.5;
+
+        Object.values(boundingBoxes).forEach((chara) => {
+            ctx.strokeRect(chara.x, chara.y, chara.w, chara.h);
+        });
+    }, []);
+
     return (
-        <div className="canvas-container">
-            <MenuContext photoImg={img} />
-        </div>
+        <>
+            <div className="canvas-container">
+                <MenuContext photoImg={img} />
+            </div>
+            <canvas id="canvas" width={1920} height={1870}></canvas>
+        </>
     );
 }
 
