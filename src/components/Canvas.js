3d0e0cdaa9487bc70581e3f1f0fa777013e754da
Luoxi Pan <l.panpax@gmail.com>
2023-03-15 12:33:34 +1100

    Remove defined bounding boxes from Canvas

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -4,35 +4,6 @@ import wallyPhoto from '../assets/photos/steampunk-wally.jpeg';
 import MenuContext from './MenuContext';
 import FirestoreFactory from '../Firestore';
 
-const boundingBoxes = {
-    deimos: {
-        x: 502,
-        y: 192,
-        w: 120,
-        h: 160,
-    },
-
-    snuffy: {
-        x: 392,
-        y: 962,
-        w: 50,
-        h: 50,
-    },
-
-    leo: {
-        x: 648,
-        y: 1051,
-        w: 80,
-        h: 60,
-    },
-    wally: {
-        x: 1602,
-        y: 683,
-        w: 20,
-        h: 20,
-    },
-};
-
 function Canvas() {
     const img = <img src={wallyPhoto} alt="" />;
 
