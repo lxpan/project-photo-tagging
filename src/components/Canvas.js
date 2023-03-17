128ff45017c11fd91edcef48ac3e6ac0d7d4a4e9
Luoxi Pan <l.panpax@gmail.com>
2023-03-17 12:21:26 +1100

    Move app logic from MenuContext into parent Canvas

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -5,10 +5,32 @@ import MenuContext from './MenuContext';
 import FirestoreFactory from '../Firestore';
 
 function Canvas() {
-    const img = <img src={wallyPhoto} alt="" />;
+    const [clicked, setClicked] = useState(false);
+    const [points, setPoints] = useState({
+        x: 0,
+        y: 0,
+    });
+    const fs = new FirestoreFactory('characters');
+
+    const validateCharAtLoc = async (e) => {
+        const toTitleCase = (name) => name[0].toUpperCase() + name.slice(1);
+
+        const charName = e.currentTarget.id;
+        const selectionCircleCssRadius = 25;
+
+        const result = await fs.isCharAtLoc(charName, points.x, points.y, selectionCircleCssRadius);
+
+        console.log(`Is ${charName} here? ${result}`);
+        if (result === true) {
+            alert(`You have found ${toTitleCase(charName)}!`);
+        }
+        else {
+            alert(`${toTitleCase(charName)} is not here!`);
+        }
+    };
 
     useEffect(() => {
-        const fs = new FirestoreFactory('characters');
+        // const fs = new FirestoreFactory('characters');
         const canvas = document.getElementById('canvas');
         const ctx = canvas.getContext('2d');
 
@@ -33,10 +55,33 @@ function Canvas() {
         // });
     }, []);
 
+    useEffect(() => {
+        const handleClick = () => setClicked(false);
+
+        window.addEventListener('click', handleClick);
+        return () => {
+            window.removeEventListener('click', handleClick);
+        };
+    }, []);
+
     return (
         <>
-            <div className="canvas-container">
-                <MenuContext photoImg={img} />
+            <div
+                className="canvas-container"
+                onContextMenu={(e) => {
+                    e.preventDefault();
+                    setClicked(true);
+                    setPoints({
+                        x: e.pageX,
+                        y: e.pageY,
+                    });
+                    // NB: the cursor location assigned to points state should be used above
+                    // and NOTE the cursor locations at validateCharAtLoc (event object from <li>)
+                    console.log('Right Click', e.pageX, e.pageY);
+                }}
+            >
+                <img src={wallyPhoto} alt="" />
+                {clicked && <MenuContext points={points} validateCharAtLoc={validateCharAtLoc} />}
             </div>
             <canvas id="canvas" width={1920} height={1870}></canvas>
         </>
--- src/components/MenuContext.js
+++ src/components/MenuContext.js
@@ -1,76 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import FirestoreFactory from '../Firestore';
-import ContextMenu from '../styles/styles';
+import React from 'react';
+import ContextMenu from '../styles/MenuContextStyle';
 
-function MenuContext({ photoImg }) {
-    const fs = FirestoreFactory('characters');
-
-    const [clicked, setClicked] = useState(false);
-    const [points, setPoints] = useState({
-        x: 0,
-        y: 0,
-    });
-
-    useEffect(() => {
-        const handleClick = () => setClicked(false);
-        window.addEventListener('click', handleClick);
-        return () => {
-            window.removeEventListener('click', handleClick);
-        };
-    }, []);
-
-    const validateCharAtLoc = async (e) => {
-        const toTitleCase = (name) => name[0].toUpperCase() + name.slice(1);
-
-        const charName = e.currentTarget.id;
-        const selectionCircleCssRadius = 25;
-
-        const result = await fs.isCharAtLoc(charName, points.x, points.y, selectionCircleCssRadius);
-
-        console.log(`Is ${charName} here? ${result}`);
-        if (result === true) {
-            alert(`You have found ${toTitleCase(charName)}!`);
-        }
-        else {
-            alert(`${toTitleCase(charName)} is not here!`);
-        }
-    };
-
+function MenuContext({ points, validateCharAtLoc }) {
     return (
-        <div
-            className="canvas-container"
-            onContextMenu={(e) => {
-                e.preventDefault();
-                setClicked(true);
-                setPoints({
-                    x: e.pageX,
-                    y: e.pageY,
-                });
-                // NB: the cursor location assigned to points state should be used above
-                // and NOTE the cursor locations at validateCharAtLoc (event object from <li>)
-                console.log('Right Click', e.pageX, e.pageY);
-            }}
-        >
-            {photoImg}
-            {clicked && (
-                <ContextMenu top={points.y - (50 + 50)} left={points.x}>
-                    <ul>
-                        <li onClick={validateCharAtLoc} id="wally">
-                            wally ⛑️
-                        </li>
-                        <li onClick={validateCharAtLoc} id="deimos">
-                            deimos 😈
-                        </li>
-                        <li onClick={validateCharAtLoc} id="snuffy">
-                            snuffy 🐈
-                        </li>
-                        <li onClick={validateCharAtLoc} id="leo">
-                            leo 🐈
-                        </li>
-                    </ul>
-                </ContextMenu>
-            )}
-        </div>
+        <ContextMenu top={points.y - (50 + 50)} left={points.x}>
+            <ul>
+                <li onClick={validateCharAtLoc} id="wally">
+                    wally ⛑️
+                </li>
+                <li onClick={validateCharAtLoc} id="deimos">
+                    deimos 😈
+                </li>
+                <li onClick={validateCharAtLoc} id="snuffy">
+                    snuffy 🐈
+                </li>
+                <li onClick={validateCharAtLoc} id="leo">
+                    leo 🐈
+                </li>
+            </ul>
+        </ContextMenu>
     );
 }
 
